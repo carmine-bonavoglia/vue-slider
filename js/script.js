ef2f21330e414f5b94305a70bee1d22a01738c72
Carmine Bonavoglia <cogitoergosumpoco@gmail.com>
2021-03-22 23:34:11 +0100

    Nothing Else Matters

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+/* 
+Descrizione: Rifare l'esercizio dello slider come fatto assieme in classe.
+Bonus1: Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
+Bonus2: Introdurre l' evento click sui pallini e visualizzare immagine relativa.
+Bonus3: spazio alla fantasia
+*/
+
 var app = new Vue({
   el: '#app',
   
